acdad38d70bd7c33912f2a113f91b61cc506f9bf
Duff OMelia <duff@omelia.org>
2012-08-13 10:05:05 -0400

    Adapt Ripple::Document support to the latest.

--- lib/awesome_print/ext/ripple_document.rb
+++ lib/awesome_print/ext/ripple_document.rb
@@ -1,35 +1,34 @@
-module AwesomePrintRippleDocument
+module AwesomePrint
+  module RippleDocument
+    def self.included(base)
+      base.send :alias_method, :cast_without_ripple_document, :cast
+      base.send :alias_method, :cast, :cast_with_ripple_document
+    end
 
-  def self.included(base)
-    base.send :alias_method, :printable_without_ripple_document, :printable
-    base.send :alias_method, :printable, :printable_with_ripple_document
-  end
+    def cast_with_ripple_document(object, type)
+      cast = cast_without_active_record(object, type)
+      return cast if !defined?(Ripple::Document)
 
-  def printable_with_ripple_document(object)
-    printable = printable_without_ripple_document(object)
-    return printable if !defined?(Ripple::Document)
-
-    if printable == :self
       if object.is_a?(Ripple::Document)
-        printable = :ripple_document_instance
+        cast = :ripple_document_instance
+      elsif object.is_a?(Class) && object.ancestors.include?(Ripple::Document)
+        cast = :ripple_document_class
       end
-    elsif printable == :class and object.ancestors.include?(Ripple::Document)
-      printable = :ripple_document_class
+      cast
     end
-    printable
-  end
 
-  def awesome_ripple_document_instance(object)
-    "#{object} " + awesome_hash(object.attributes.merge(:key => object.key))
-  end
-
-  def awesome_ripple_document_class(object)
-    data = object.properties.inject(ActiveSupport::OrderedHash.new) do |hash, p|
-      hash[p.first.to_sym] = p.last.type.to_s
-      hash
+    def awesome_ripple_document_instance(object)
+      "#{object} " + awesome_hash(object.attributes.merge(:key => object.key))
     end
-    "class #{object} < #{object.superclass} " << awesome_hash(data)
+
+    def awesome_ripple_document_class(object)
+      data = object.properties.inject(ActiveSupport::OrderedHash.new) do |hash, p|
+        hash[p.first.to_sym] = p.last.type.to_s
+        hash
+      end
+      "class #{object} < #{object.superclass} " << awesome_hash(data)
+    end
   end
 end
 
-AwesomePrint.send(:include, AwesomePrintRippleDocument)
+AwesomePrint::Formatter.send(:include, AwesomePrint::RippleDocument)
